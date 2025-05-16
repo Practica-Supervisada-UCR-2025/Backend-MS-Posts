c8ff36e73d7fa45728a4f8c9ed82e8114e5983a5
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2025-05-16 02:25:27 -0600

    feat(reported-posts): recover reported posts route

--- src/features/posts/routes/reportedPosts.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router, RequestHandler } from 'express';
-import { getUserPostsController } from '../controllers/userPosts.controller';
-import { authenticateJWT } from '../../middleware/authenticate.middleware';
-
-const router = Router();
-
-router.get('/posts/reported', authenticateJWT, getUserPostsController as RequestHandler);
-
-export default router;
