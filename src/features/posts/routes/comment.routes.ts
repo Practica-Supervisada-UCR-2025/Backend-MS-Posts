18e56e4a6afc2fce7693716976b90133a857fec5
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2025-06-27 02:47:43 -0600

    feat: added suspension middleware to comment and post routes

--- src/features/posts/routes/comment.routes.ts
+++ src/features/posts/routes/comment.routes.ts
@@ -1,12 +1,13 @@
 import { Router ,RequestHandler} from 'express';
 import { authenticateJWT } from '../../middleware/authenticate.middleware';
+import { checkUserSuspension } from '../../middleware/suspension.middleware';
 import { createCommentController, getPostCommentsController } from '../controllers/commentCrud.controller';
 
 const router = Router();
 
 // Create a new comment on a post (protected by JWT)
-router.post('/posts/newComment', authenticateJWT, createCommentController as RequestHandler);
+router.post('/posts/newComment', authenticateJWT, checkUserSuspension, createCommentController as RequestHandler);
 
-router.get('/posts/:postId/comments', authenticateJWT, getPostCommentsController as RequestHandler);
+router.get('/posts/:postId/comments', authenticateJWT, checkUserSuspension, getPostCommentsController as RequestHandler);
 
 export default router;
--- src/features/posts/routes/post.routes.ts
+++ src/features/posts/routes/post.routes.ts
@@ -3,32 +3,33 @@ import { authenticateJWT } from '../../middleware/authenticate.middleware';
 import { getUserPostsController, getPostsByUserIdController, getPostByIdController } from '../controllers/getPosts.controller';
 import { deleteOwnPostController } from '../controllers/post.controller';
 import { getReportedPostsController } from "../controllers/reportedPosts.controller";
+import { checkUserSuspension } from '../../middleware/suspension.middleware';
 import { createPostController, getPostsFeedController } from '../controllers/postCrud.controller';
 
 const router = Router();
 
 // GET
 // Get user posts route (protected by JWT)
-router.get('/user/posts/mine', authenticateJWT, getUserPostsController as RequestHandler);
+router.get('/user/posts/mine', authenticateJWT, checkUserSuspension, getUserPostsController as RequestHandler);
 
 // Get posts for a specific user by UUID route (protected by JWT)
-router.get('/posts/user/:uuid', authenticateJWT, getPostsByUserIdController as RequestHandler);
+router.get('/posts/user/:uuid', authenticateJWT, checkUserSuspension, getPostsByUserIdController as RequestHandler);
 
 // Get all reported posts route (protected by JWT)
 router.get('/posts/reported', authenticateJWT, getReportedPostsController as RequestHandler);
 
 // Get posts feed route (protected by JWT)
-router.get('/posts/feed', authenticateJWT, getPostsFeedController as RequestHandler)
+router.get('/posts/feed', authenticateJWT, checkUserSuspension, getPostsFeedController as RequestHandler)
 
 // DELETE
 // Delete own post route (protected by JWT)
-router.delete('/user/posts/delete/:postId', authenticateJWT, deleteOwnPostController as unknown as RequestHandler);
+router.delete('/user/posts/delete/:postId', authenticateJWT, checkUserSuspension, deleteOwnPostController as unknown as RequestHandler);
 
 // POST
 // Create a new post (protected by JWT)
-router.post('/posts/newPost', authenticateJWT, createPostController as RequestHandler);;
+router.post('/posts/newPost', authenticateJWT, checkUserSuspension, createPostController as RequestHandler);;
 
 // Get post by ID route (protected by JWT)
-router.get('/user/posts/:postId', authenticateJWT, getPostByIdController as RequestHandler);
+router.get('/user/posts/:postId', authenticateJWT, checkUserSuspension, getPostByIdController as RequestHandler);
 
 export default router;
